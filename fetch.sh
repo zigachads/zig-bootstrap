05742dadf44b3d2679592b9f6a20ea9b4e2e9564
Theo Paris <theop@flowtr.dev>
2024-02-03 20:33:30 -0800

    chore(deps): update dependencies

--- fetch.sh
+++ fetch.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-LLVM_VERSION=${LLVM_VERSION:-"llvmorg-17.0.0-rc4"}
-ZIG_BRANCH=${ZIG_BRANCH:-"llvm17"}
+LLVM_VERSION=${LLVM_VERSION:-"llvmorg-17.0.6"}
+ZIG_BRANCH=${ZIG_BRANCH:-"master"}
 GIT_ARGS=${GIT_ARGS:-"--depth=1"}
 
-if ! [ -x "$(command -v git)" ]; then
+if ! test -x "$(command -v git)"; then
 	echo "Git is required." >&2
 	exit 1
 fi
 
-git clone $GIT_ARGS https://github.com/zlib-ng/zlib-ng ./zlib-ng
-git clone $GIT_ARGS https://github.com/facebook/zstd zstd
-git clone $GIT_ARGS -b "$ZIG_BRANCH" https://github.com/ziglang/zig ./zig
-git clone $GIT_ARGS -b "$LLVM_VERSION" https://github.com/llvm/llvm-project ./llvm-project
+git clone $GIT_ARGS https://code.flowtr.dev/mirrors/zlib-ng ./zlib-ng
+git clone $GIT_ARGS https://code.flowtr.dev/mirrors/zstd zstd
+git clone $GIT_ARGS -b "$ZIG_BRANCH" https://code.flowtr.dev/mirrors/zig ./zig
+git clone $GIT_ARGS -b "$LLVM_VERSION" https://code.flowtr.dev/mirrors/llvm-project ./llvm-project
 
